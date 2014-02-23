d5e3339cfc13ad1f2eea035e924b55ec9384312c
ivanir <faridescate@gmail.com>
2014-02-23 09:46:34 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CineChevere::Application.config.secret_key_base = '6a453c593363e40375f7ac0300c6c9175dfa41ce60968ac1d38b312b49dca7fb4d512ecd568b13945a50f8f4347ca53895c8f6b65c4afafe4c347805b505e055'
